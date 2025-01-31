224dea54f1b5149fd11312a9b09d9a25e601653a
vlad <134726680+dark-azy@users.noreply.github.com>
2025-01-31 11:05:35 +0100

    Update Grille.h

--- Grille.h
+++ Grille.h
@@ -1,29 +1,27 @@
 #pragma once
 #include <vector>
 #include <iostream>
-
 #include "Cellule.h"
 
 class Grille
 {
-	protected:
-	int x=0;
-	int y=0;
-	std::string lecture;
-	std::string ecriture;
-	std::string ligne;
-	public:
-	Grille() = default;
-	Grille(std::string lecture, std::string ecriture);
-	virtual void iteration(int n);
-	virtual void afficher_Grille();
-	virtual void lireFichier();
-	virtual void ecrireFichier();
-	virtual void clear();
-	//std::vector<Cellule>& return_cellules();
-	virtual bool verifierSolution(const std::string& cheminSolution);// testUnitaire
+    protected:
+    int x=0;
+    int y=0;
+    std::string lecture;
+    std::string ecriture;
+    std::string ligne;
+    public:
+    
+    std::vector<Cellule> cellules;
+
+    Grille() = default;
+    Grille(std::string lecture);
+    virtual void iteration(int n, bool choix);
+    virtual void afficher_Grille();
+    virtual void lireFichier();
+    virtual void ecrireFichier(int a);
+    virtual void clear();
+    //std::vector<Cellule>& return_cellules();
+    virtual bool verifierSolution(const std::string& cheminSolution);// testUnitaire
 };
-
-
-
-
