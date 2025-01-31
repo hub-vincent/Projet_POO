2523cb691b370f95d25aedc9b0f5c554422c5a7e
vlad <134726680+dark-azy@users.noreply.github.com>
2025-01-31 11:06:13 +0100

    Update GrilleTorique.h

--- GrilleTorique.h
+++ GrilleTorique.h
@@ -1,15 +1,10 @@
 #pragma once
 #include "Grille.h"
 
-
 class GrilleTorique : public Grille
 {
-	public:
-	void iteration(int n) override;
-	void ecrireFichier() override;
-	void lireFichier() override;
-	GrilleTorique(std::string lecture, std::string ecriture);
-	void afficher_Grille() override;
+    public:
+    void iteration(int n, bool choix) override;
+    GrilleTorique(std::string lecture);
 
 };
-
