30dc75c4c276fbff91e457a8df18a0a9ea421a7f
vlad <134726680+dark-azy@users.noreply.github.com>
2025-01-31 11:05:07 +0100

    Update Cellule.h

--- Cellule.h
+++ Cellule.h
@@ -1,21 +1,19 @@
 #pragma once
-#include "Grille.h"
+#include <iostream>
 
 class Cellule
 {
 private:
-	int index;
-	int x;
-	int y;
-	bool val;
+    int index;
+    int x;
+    int y;
+    bool val;
 public:
-	Cellule(int index, bool val, int X_max);
-	void afficher() const;
-	int return_index() const;
-	int return_x() const;
-	int return_y() const;
-	int return_state() const;
-	void change_state(bool u);
+    Cellule(int index, bool val, int X_max);
+    void afficher() const;
+    int return_index() const;
+    int return_x() const;
+    int return_y() const;
+    int return_state() const;
+    void change_state(bool u);
 };
-
-
